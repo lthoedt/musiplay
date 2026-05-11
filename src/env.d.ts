e3fa13d93307e8497d64c848be05c7360a70470c
Leander ten Hoedt <lthoedt@gmail.com>
2026-05-11 17:15:54 +0200

    ~ more init changes

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,19 +1,24 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/client" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue';
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
+declare module "*.vue" {
+	import type { DefineComponent } from "vue";
+	const component: DefineComponent<{}, {}, any>;
+	export default component;
 }
 
-declare module 'virtual:pwa-register' {
-  export function registerSW(options?: {
-    immediate?: boolean;
-    onNeedRefresh?: () => void;
-    onOfflineReady?: () => void;
-    onRegistered?: (registration: ServiceWorkerRegistration | undefined) => void;
-    onRegisteredSW?: (swScriptUrl: string, registration: ServiceWorkerRegistration | undefined) => void;
-    onRegisterError?: (error: unknown) => void;
-  }): (reloadPage?: boolean) => Promise<void>;
+declare module "virtual:pwa-register" {
+	export function registerSW(options?: {
+		immediate?: boolean;
+		onNeedRefresh?: () => void;
+		onOfflineReady?: () => void;
+		onRegistered?: (
+			registration: ServiceWorkerRegistration | undefined,
+		) => void;
+		onRegisteredSW?: (
+			swScriptUrl: string,
+			registration: ServiceWorkerRegistration | undefined,
+		) => void;
+		onRegisterError?: (error: unknown) => void;
+	}): (reloadPage?: boolean) => Promise<void>;
 }
